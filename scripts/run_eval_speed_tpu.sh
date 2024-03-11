6114298b713e06a6f8b010bb4bb1d9ea424b0e91
Jiarui Xu <xvjiarui0826@gmail.com>
2024-03-10 17:10:05 -0700

    change chunk_size to 256

--- scripts/run_eval_speed_tpu.sh
+++ scripts/run_eval_speed_tpu.sh
@@ -9,10 +9,10 @@ export llama_tokenizer_path="/home/xvjiarui0826/LWM/checkpoints/LWM-Text-Chat-1M
 export lwm_text_checkpoint="/home/xvjiarui0826/LWM/checkpoints/LWM-Text-Chat-1M-Jax/params"
 
 python3 -u scripts/eval_speed.py \
-    --mesh_dim='!1,1,8,-1' \
+    --mesh_dim='!1,1,32,-1' \
     --dtype='fp32' \
     --load_llama_config='7b' \
-    --update_llama_config="dict(theta=50000000,max_sequence_length=1048576,scan_attention=True,scan_query_chunk_size=1024,scan_key_chunk_size=1024,scan_mlp=True,scan_mlp_chunk_size=1024,scan_layers=True)" \
+    --update_llama_config="dict(theta=50000000,max_sequence_length=1048576,scan_attention=True,scan_query_chunk_size=256,scan_key_chunk_size=256,scan_mlp=True,scan_mlp_chunk_size=1024,scan_layers=True)" \
     --load_checkpoint="params::$lwm_text_checkpoint" \
     --tokenizer.vocab_file="$llama_tokenizer_path" \
     --max_tokens_per_batch=1000000 \
